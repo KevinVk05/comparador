29a6109f1b0bb6ff72d61645938a616df59a55f9
Kevin_IH <152276338+KevinVk05@users.noreply.github.com>
2025-06-04 15:48:50 +0200

    Update http-externo.js

--- src/servicios/conexionAxios/http-externo.js
+++ src/servicios/conexionAxios/http-externo.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpExterno = axios.create({
-  baseURL: 'http://localhost:8080/', 
+  baseURL: 'https://comparador-9aa3d8c8d0b3.herokuapp.com', 
   headers: {
     'Content-Type': 'application/json',
   },
